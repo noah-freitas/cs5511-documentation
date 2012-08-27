f31eae8534c19a1e1be86827939c85d67782fc5a
Jay Peretz <jay@mytowncontractor.com>
2012-08-27 13:34:34 -0700

    working on project 1

--- projects/programming/programming.js
+++ projects/programming/programming.js
@@ -1,7 +1,10 @@
 // JavaScript Document
+// access the grid element - returns a single object
 var grid = document.getElementById("grid");
-var spans = grid.getElementsByClassName("span2");
+// access the classes within grid - returns a set of objects
+var spans = grid.getElementsByClassName("span1");
 var rows = grid.getElementsByClassName("row");
+// add a paragraph of text within each block of the grid
 for (x in spans)
     {
 		spans[x].innerHTML="<p>"+ x +"</p>";
@@ -24,5 +27,4 @@ do {
 	spans[x].style.padding = "10px auto;";
 	spans[x].style.color = "white";
 	x++;
-	console.log(x);
 } while (x < spans.length);
