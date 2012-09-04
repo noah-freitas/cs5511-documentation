14910fc75576ac90be23be87a7a8d4f392ef30e5
Noah Freitas <noah.j.freitas@gmail.com>
2012-09-04 11:44:08 -0700

    Did a test walk-through of project 2.
    I added my documents under solutions/naptime/test/
    
    I made some minor modifications to the project page:
    
    * Included CSS rules in HTML skeleton.
    * Updated page title.
    * Synchronized audio tag ID name.
    * Removed unnecessary function literals from addEventListener and setInterval calls.
    * Changed == to ===.
    * Standardized whitespace.

---
+++ solutions/naptime/test/timer.js
@@ -0,0 +1,41 @@
+var timer = new Date();
+var seconds = document.getElementById('seconds');
+var dashboard = document.getElementById('dashboard');
+var wakeup = document.getElementById('wakeup');
+wakeup.style.display = "none";
+var start = document.getElementById('start');
+start.addEventListener('click', takeNap);
+function takeNap() {
+    timer.setSeconds(seconds.value);
+    setInterval(alarm, 1000);
+    start.style.display = "none";
+    onOff.style.display = "inline";
+    playMusic();
+}
+function alarm() {
+    timer.setSeconds(timer.getSeconds()-1);
+    seconds.value = timer.getSeconds();
+    if (timer.getSeconds() === 0) {
+        wakeup.style.display = "block";
+        dashboard.style.display = "none";
+        audioElement.pause();		
+    }
+}
+
+var audioElement = document.getElementById('audiotag1');
+var onOff = document.getElementById("toggle");
+onOff.style.display = "none";
+onOff.addEventListener('click', pausePlay);
+audioElement.addEventListener('ended', playMusic);
+function playMusic() {
+    audioElement.play();
+}
+function pausePlay() {
+    if (onOff.getAttribute("value") === "Pause Music") {
+        audioElement.pause();
+        onOff.setAttribute("value","Resume Music");
+    } else {		
+        audioElement.play();
+        onOff.setAttribute("value","Pause Music");
+    }
+}
