f7c9b15ec9fce1998f90900f074c1ab52300d643
Jay Peretz <jay@mytowncontractor.com>
2012-09-30 11:07:07 -0700

    added proj 4, updated home, started proj 5

---
+++ examples/registration/classes.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+var classes = {
+	class: {
+	code:"CS55.11",
+	title:"Javascript & JQuery",
+	description:"Introduction to programming with Javascript.",
+	sections: {
+		section: {
+			sectionNumber : 1371,
+			day : ["Tuesday"]
+		}
+	}
+	},
+	class: {
+	code:"CS10",
+	title:"Introduction to Computer Science",
+	description:"Introduction to programming with C++.",
+	sections: {
+		section: {
+			sectionNumber : xxxx,
+			day : ["Monday"]
+		}
+	}
+	}	
+	};
