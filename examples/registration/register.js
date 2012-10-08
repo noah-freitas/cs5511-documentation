2d1b0cd5cf047286c3193dc83fd02a3926fe7201
Jay Peretz <jay@mytowncontractor.com>
2012-10-08 09:28:54 -0700

    jquery lesson, update site, restarting proj 5

--- examples/registration/register.js
+++ examples/registration/register.js
@@ -2,24 +2,45 @@
  $(document).ready(function() {
 	 var ii=0,
 	 len=0,
-	 classes = [];
-  $("#sections").hide();	 
-  $.ajax({
-      url: "classes.json"
-     }).done(function(data) { 
-	 classes = JSON.parse(data);	
-	 len = classes.course.length;
-	 for (ii=0;ii<len;ii++) {
-	 $("#courses").append("<option value="+ii+" >"+classes.course[ii].description+"</option>");
-	 }  
-	 });
-	 $("#courses").change(function(e) {
-		 $("#sections").html("<option>Choose a Section</option>");
+	 classes = [],
+	 mySchedule = [],
+	 courseNumber;	 
+	 $("#sections").hide();	 
+     loadClasses("classes.json");
+	 console.log(this);
+	
+	 displayCourses();
+	
+	 /*$("#courses").change(function(e) {
+		$("#sections").html("<option>Choose a Section</option>");
 		 len = classes.course[e.target.value].section.length;
 		 ii=0;
 		  for (ii=0;ii<len;ii++) {
-	 $("#sections").append("<option value="+ii+" >"+classes.course[e.target.value].section[ii].instructor+"</option>");
-	 }	
-	  $("#sections").show();	
+	           $("#sections").append("<option value="+ii+" >"+classes.course[e.target.value].section[ii].instructor+"</option>");
+	      }	
+		  courseNumber=e.target.value;
+		  mySchedule.push(classes.course[e.target.value]);
+		  
+	    $("#sections").show();	
 	 });
+	  $("#sections").change(function(e) {
+		  mySchedule[mySchedule.length-1].section = classes.course[courseNumber].section[e.target.value];
+		   $('#schedule > tbody').append('<tr><td>'+mySchedule[mySchedule.length-1].description+'</td><td>'+mySchedule[mySchedule.length-1].section.day   +'</td> </tr>');
+	  });*/
+	  
+
+ function displayCourses() {
+	     len = classes.course.length;
+	     for (ii=0;ii<len;ii++) {			 
+	           $("#courses").append("<option value="+ii+" >"+classes.course[ii].description+"</option>");
+	      } 
+ }
+ function loadClasses(CSSchedule) {
+	 var that = this;
+	
+	  $.getJSON(CSSchedule,function(data) {           
+	      that.classes = data;
+		 
+	 });
+ } 
  });
